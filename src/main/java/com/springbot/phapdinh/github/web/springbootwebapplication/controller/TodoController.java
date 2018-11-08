a32bc6275e8bf176f378bf9e5690850bf3943814
phapdinh <centralparkloop@yahoo.com>
2018-11-07 20:33:11 -0500

    add ability to change target date

--- src/main/java/com/springbot/phapdinh/github/web/springbootwebapplication/controller/TodoController.java
+++ src/main/java/com/springbot/phapdinh/github/web/springbootwebapplication/controller/TodoController.java
@@ -1,17 +1,21 @@
 package com.springbot.phapdinh.github.web.springbootwebapplication.controller;
 
+import java.text.SimpleDateFormat;
 import java.util.Date;
 
 import javax.validation.Valid;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.propertyeditors.CustomDateEditor;
 import org.springframework.ui.ModelMap;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.SessionAttributes;
 
 import com.springbot.phapdinh.github.web.springbootwebapplication.service.TodoService;
@@ -22,6 +26,12 @@ import com.springbot.phapdinh.github.web.springbootwebapplication.model.Todo;
 public class TodoController {
   @Autowired
   private TodoService todos;
+
+  @InitBinder
+  public void initBinder(WebDataBinder binder) {
+    SimpleDateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy");
+    binder.registerCustomEditor(Date.class, new CustomDateEditor(dateFormat, false));
+  }
 
   @RequestMapping(value="/list-todos", method=RequestMethod.GET)
   public String showTodos(ModelMap model) {
@@ -41,7 +51,7 @@ public class TodoController {
     if(result.hasErrors()) {
 			return "todo";
 		}
-    todos.addTodo((String) model.get("name"), todo.getDesc(), new Date(), false);
+    todos.addTodo((String) model.get("name"), todo.getDesc(), todo.getTargetDate(), false);
     return "redirect:/list-todos";
   }
 
