53d27de0921132b1831515fa74d6347e476da68c
Phap <centralparkloop@yahoo.com>
2020-04-03 10:58:09 -0400

    use different annotations

--- src/main/java/com/springbot/phapdinh/github/web/springbootwebapplication/controller/LoginController.java
+++ src/main/java/com/springbot/phapdinh/github/web/springbootwebapplication/controller/LoginController.java
@@ -3,9 +3,9 @@ package com.springbot.phapdinh.github.web.springbootwebapplication.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.ui.ModelMap;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.SessionAttributes;
 import com.springbot.phapdinh.github.web.springbootwebapplication.service.LoginService;
 
@@ -15,12 +15,12 @@ public class LoginController {
   @Autowired
   private LoginService service;
 
-  @RequestMapping(value={"/login", "/"}, method=RequestMethod.GET)
+  @GetMapping(value={"/login", "/"})
   public String showLogin() {
     return "login";
   }
 
-  @RequestMapping(value="/login", method=RequestMethod.POST)
+  @PostMapping(value="/login")
   public String showWelcomePage(@RequestParam String name, @RequestParam String password, ModelMap model) {
     boolean isValidUser = service.validateUser(name, password);
     if(isValidUser) {
@@ -32,7 +32,7 @@ public class LoginController {
     }
   }
 
-  @RequestMapping(value="/intro", method=RequestMethod.GET)
+  @GetMapping(value="/intro")
   public String introductionMessage(@RequestParam String name, ModelMap model) {
     model.put("name", name);
     return "intro";
